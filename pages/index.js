0b314e0f83db71d3c8850b29b25bee813e616bcd
yellowhappeicco <qrockse@gmail.com>
2022-09-04 13:55:18 +0900

    change "Readpost"

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { getDatabase } from "../lib/notion";
 import { Text } from "./[id].js";
 import styles from "./index.module.css";
+import { GA_ID } from "../lib/gtag";
 
 export const databaseId = process.env.NOTION_DATABASE_ID;
 
@@ -12,9 +13,9 @@ export default function Home({ posts }) {
       <Head>
         <title>Test</title>
         <link rel="icon" href="/favicon.ico" /> {/* faviconの設定*/}
-      </Head>
-
-      <main className={styles.container}>
+    </Head>
+    
+    <main className={styles.container}>
         <header className={styles.header}>
           <h1>Test</h1>
           <p>
@@ -22,7 +23,7 @@ export default function Home({ posts }) {
           </p>
         </header>
 
-        <h2 className={styles.heading}>All Posts</h2>
+        <h2 className={styles.heading}>LIST</h2>
         <ol className={styles.posts}>
           {posts.map((post) => {
             const date = new Date(post.last_edited_time).toLocaleString(
@@ -32,7 +33,7 @@ export default function Home({ posts }) {
                 day: "2-digit",
                 year: "numeric",
               }
-            ); {/* 日付の形式を整える。ja-JP->日本語を指定*/}
+            ); { /* 日付の形式を整える。ja-JP->日本語を指定*/ }
             return (
               <li key={post.id} className={styles.post}>
                 <h3 className={styles.postTitle}>
@@ -45,7 +46,7 @@ export default function Home({ posts }) {
 
                 <p className={styles.postDescription}>{date}</p>
                 <Link href={`/${post.id}`}>
-                  <a> Read post →</a>
+                  <a> Check→</a>
                 </Link>
               </li>
             );
