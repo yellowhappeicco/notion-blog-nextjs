b4bce25b5d6f3dddd2fe7093a81960a657d3a13b
yellowhappeicco <qrockse@gmail.com>
2022-09-10 00:02:42 +0900

    _app.jsの修正

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,7 @@
 import "../styles/globals.css";
-import React from 'react'
 
-import GoogleAnalytics from '../src/components/GoogleAnalytics'
-
-const App = ({ Component, pageProps }) => {
-  return (
-    <>
-      <GoogleAnalytics />
-
-      <Component {...pageProps} />
-    </>
-  )
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
 
-export default App
+export default MyApp;
