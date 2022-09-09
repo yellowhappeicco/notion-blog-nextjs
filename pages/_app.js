1eed211692cabc76d015c97c387651ce1fd97345
yellowhappeicco <qrockse@gmail.com>
2022-09-10 08:39:51 +0900

    GA追記

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,37 @@
-import "../styles/globals.css";
+// このファイルの中に書かれていることが全ページに適用される
+import "../styles/globals.css"; // 全ページにglobl.cssに書いたスタイルが適用されます
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import Head from "next/head";
+import React from 'react'
+
+import { GA_ID, existsGaId } from '../src/lib/gtag'
+
+const App = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Head>
+        {/* Google Analytics */}
+        {existsGaId && (
+          <>
+            <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${GA_ID}', {
+                    page_path: window.location.pathname,
+                  });`,
+              }}
+            />
+          </>
+        )}
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
 
-export default MyApp;
+export default App
